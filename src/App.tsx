c2b1cf94f968b28e554e09e7e82b7bf4505ea331
Gergely Kosy <gergelykosy@gmail.com>
2025-01-23 16:59:01 +0100

    App updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import BlogPage from "./components/PageTypes/BlogPage";
 import "./styles/main.scss";
 import HomePage from "./components/PageTypes/HomePage";
-import Home from "./pages/Home";
 import GeneralPage from "./components/PageTypes/GeneralPage";
 
 function App() {
@@ -15,9 +13,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/generic" element={<GeneralPage />} />
-            <Route path="/blog/first" element={<BlogPage />} />
+            <Route path="/about" element={<GeneralPage />} />
+            <Route path="/contact" element={<GeneralPage />} />
           </Routes>
         </main>
         <Footer />
